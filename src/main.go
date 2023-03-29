5a0004f031d0f34e3a1a69931c69a0288f36dac3
Daan <daandenhoed@gmail.com>
2023-03-29 09:10:54 +0200

    db connect

--- src/main.go
+++ src/main.go
@@ -1,40 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
-  "database/sql"
-  "github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
-
 const (
-  host = 'localhost'
-  port = 8001
-  user = 'postgres'
-  password = 'passwd'
-  dbname = 'postgres'
+	host     = "localhost"
+	port     = 8001
+	user     = "postgres"
+	password = "passwd"
+	dbname   = "postgres"
 )
 
 func main() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-  "password=%s dbname=%s sslmode=disable",
-  host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
-  
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
-  
-  fmt.Println("db connected")
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-  router := NewRouter()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("db connected")
+	fmt.Println("api up")
+	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
-  
-  fmt.Println("api up")
+
+}
